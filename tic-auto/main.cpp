be70d72d00483354539c5d54df816111f6e7549e
DeVeL_AnOnIk <27363518+anonik9900@users.noreply.github.com>
2021-07-04 18:39:20 +0200

    update v2.0

--- tic-auto/main.cpp
+++ tic-auto/main.cpp
@@ -10,6 +10,19 @@ string DRM;
 string games;
 string language;
 string game_size;
+string multi_enabled;
+string is_dlc_aviable;
+string dlc_numbers;
+
+//DLC NUMBER
+string dlc_id1;
+string dlc_id2;
+string dlc_id3;
+string dlc_id4;
+string dlc_id5;
+string dlc_id6;
+string dlc_id7;
+int x,y,z,w,l,m,n,o,p,q,r,s,t,u;
 int main()
 {
 cout<<"TTTTTTTTTTTTTTTTTTTTTTT        IIIIIIIIII                CCCCCCCCCCCCC"<<endl;
@@ -50,6 +63,70 @@ cout<<endl;
 cout<<" Quanto pesa il gioco ?"<<endl;
 cin>>game_size;
 cout<<endl;
+
+cout<<" Il multiplayer funziona ?"<<endl;
+cin>>multi_enabled;
+cout<<endl;
+
+dlc:
+cout<<" Sono disponibili DLC ?"<<endl;
+cin>>is_dlc_aviable;
+cout<<endl;
+
+//YES DLC
+
+if (is_dlc_aviable == "yes", is_dlc_aviable =="si")
+{
+    cout<<"Quanti dlc ci sono ?"<<endl;
+    cin>>dlc_numbers;
+    cout<<endl;
+    if (dlc_numbers == "1")
+    {
+        cout<<" Inserisci il nome del dlc oppure inserisci semplicemente l'ID del dlc "<<endl;
+        cin>>dlc_id1;
+        cout<<endl;
+    }
+
+    else if (dlc_numbers  =="2")
+    {
+        cout<<" Inserisci il nome del primo dlc oppure inserisci semplicemente l'ID del dlc "<<endl;
+        cin>>dlc_id1;
+        cout<<endl;
+
+        cout<<" Inserisci il nome del secondo dlc oppure inserisci semplicemente l'ID del dlc "<<endl;
+        cin>>dlc_id2;
+        cout<<endl;
+    }
+
+        else if (dlc_numbers  =="3")
+    {
+        cout<<" Inserisci il nome del primo dlc oppure inserisci semplicemente l'ID del dlc "<<endl;
+        cin>>dlc_id1;
+        cout<<endl;
+
+        cout<<" Inserisci il nome del secondo dlc oppure inserisci semplicemente l'ID del dlc "<<endl;
+        cin>>dlc_id2;
+        cout<<endl;
+
+        cout<<" Inserisci il nome del terzo dlc oppure inserisci semplicemente l'ID del dlc "<<endl;
+        cin>>dlc_id3;
+        cout<<endl;
+    }
+
+    else if (dlc_numbers <="0", dlc_numbers >="8")
+    {
+        cout<<"Nessun valore inserito, torno alla scelta del dlc..."<<endl;
+        cout<<endl;
+        goto dlc;
+    }
+}
+
+//NO DLC
+else if(is_dlc_aviable == "no")
+{
+    cout<<"Perfetto passo al prossimo step..."<<endl;
+    cout<<endl;
+}
 
 string a_logo = "TTTTTTTTTTTTTTTTTTTTTTT        IIIIIIIIII                CCCCCCCCCCCCC";
 string b_logo = "T:::::::::::::::::::::T        I::::::::I             CCC::::::::::::C";
@@ -94,6 +171,25 @@ string game_size_ph = "Size: ";
 string game_size_unit ="GB";
 string game_size_result = (game_size_ph + game_size + game_size_unit);
 
+//Multiplayer
+string multi_var = "Multiplayer Enabled: ";
+string multi_result = (multi_var + multi_enabled);
+
+//Dlc
+string dlc_ci_sono = "Dlc Aviable: ";
+string dlc_ci_sono_numbers = "Numbers: ";
+
+//no dlc
+string dlc_non_ci_sono ="Dlc Aviable: ";
+string dlc_non_ci_sono_all = (dlc_non_ci_sono + is_dlc_aviable);
+
+//1dlc
+string dlc_solo_uno = (dlc_ci_sono + is_dlc_aviable);
+string dlc_solo_uno_num = (dlc_ci_sono_numbers + dlc_numbers);
+string dlc_solo_uno_nam = "DLC Names or ID: ";
+string parser =",";
+string dlc_solo_uno_nam_result = (dlc_solo_uno_nam + dlc_id1 + parser + dlc_id2 + parser + dlc_id3 + parser + dlc_id4 + parser + dlc_id5 + parser + dlc_id6 + parser + dlc_id7);
+
 //Credits
 string credits_ph = "Credits: ";
 string credits_unit = "Cracked By TIC Team For Community.";
@@ -132,6 +228,28 @@ fout <<language_result<<endl;
 fout <<endl;
 fout <<game_size_result<<endl;
 fout <<endl;
+fout <<multi_result<<endl;
+fout <<endl;
+
+
+//DLC
+
+if (is_dlc_aviable == "yes", is_dlc_aviable =="si")
+{
+    fout<<dlc_solo_uno<<endl;
+    fout<<endl;
+    fout<<dlc_solo_uno_num<<endl;
+    fout<<endl;
+    fout<<dlc_solo_uno_nam_result<<endl;
+    fout<<endl;
+}
+
+if (is_dlc_aviable == "no")
+{
+    fout<<dlc_non_ci_sono_all<<endl;
+    fout<<endl;
+}
+
 fout <<credits_result<<endl;
 fout <<endl;
 fout <<commento_sep<<endl;
